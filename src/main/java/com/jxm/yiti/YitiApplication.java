df7b7c89b1f7d423f834893017b81fbea22ad86f
JXM <jiangyk@nwafu.edu.cn>
2022-12-28 21:04:01 +0800

    init

---
+++ src/main/java/com/jxm/yiti/YitiApplication.java
@@ -0,0 +1,13 @@
+package com.jxm.yiti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YitiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YitiApplication.class, args);
+	}
+
+}
