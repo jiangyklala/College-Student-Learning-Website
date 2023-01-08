bc6fe2720c928954c6e91446ac12bedb956ff280
jiangyk <jiangyk@nwafu.edu.cn>
2023-01-08 20:29:08 +0800

    集成所有图标库, 首页布局初步

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,22 +1,95 @@
 <template>
   <a-layout-content style="padding: 0 50px">
-    <div :style="{ background: '#fff', padding: '24px', minHeight: '280px' }">Content</div>
+    <a-list
+        item-layout="horizontal"
+        :data-source="listData"
+        :pagination="pagination"
+    >
+      <template #renderItem="{ item }">
+        <a-list-item key="item.title">
+          <template #actions>
+            <span v-for="{ type, text } in actions" :key="type">
+            <component :is="type" style="margin-right: 8px"/>
+            {{ text }}
+            </span>
+            <a-button type="primary" shape="round" :size="size">
+              <template #icon>
+                <DownloadOutlined />
+              </template>
+            </a-button>
+          </template>
+          <a-list-item-meta >
+            <template #title>
+              <a :href="item.href">{{ item.title }}</a>
+            </template>
+          </a-list-item-meta>
+          {{ item.description }}
+        </a-list-item>
+      </template>
+    </a-list>
   </a-layout-content>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import axios from 'axios';
+import {defineComponent, onMounted} from 'vue';
+import {EyeTwoTone, ThunderboltTwoTone, DownloadOutlined} from '@ant-design/icons-vue';
+
+const listData : Record<string, string>[] = [];
+
+for (let i = 0; i < 23; i++) {
+  listData.push({
+    href: 'https://www.antdv.com',
+    title: `ant design vue part ${i}`,
+    description:
+      'soifjasoifj osjf oijasifjosajfoi ajsojo',
+  });
+}
 
 export default defineComponent({
+  components: {
+    EyeTwoTone,
+    ThunderboltTwoTone,
+    DownloadOutlined,
+  },
   name: 'Home',
   setup() {
-    console.log("setup");
-    axios.get("http://localhost:8880/downloadList/list?name=资料2").then(
-        (response) => {
-          console.log(response);
-        }
-    )
-  }
+    // console.log("setup");
+    // const downloadList = ref();
+
+    const pagination = {
+      onChange: (page: number) => {
+        console.log(page);
+      },
+      pageSize: 10,
+    };
+    const actions: Record<string, string>[] = [
+
+      { type: 'EyeTwoTone', text: '156' },
+      { type: 'ThunderboltTwoTone', text: '355' },
+    ];
+
+    onMounted(() => {
+      // console.log("onMounted");
+      // axios.get("http://localhost:8880/downloadList/list?name=资料2").then(
+      //     (response) => {
+      //       const data = response.data;
+      //       downloadList.value = data.content;
+      //       console.log(response);
+      //     }
+      // )
+    });
+
+
+
+    return {
+      // downloadList,listData,
+      pagination,
+      actions,
+      listData,
+    };
+  },
 });
 </script>
+
+<style scoped>
+</style>
