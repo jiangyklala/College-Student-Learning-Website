8a6ff6ff4ca9b47621acef17c8e42dbd08dac8c0
jiangyk <jiangyk@nwafu.edu.cn>
2023-04-24 18:03:32 +0800

    update

---
+++ src/main/java/com/jxm/yiti/controller/GptController.java
@@ -0,0 +1,49 @@
+package com.jxm.yiti.controller;
+
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
+import com.jxm.yiti.domain.ChatHistory;
+import com.jxm.yiti.req.ChatCplQueryReq;
+import com.jxm.yiti.resp.ChatCplQueryResp;
+import com.jxm.yiti.resp.CommonResp;
+import com.jxm.yiti.service.GptService;
+import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/gpt")
+public class GptController {
+
+    @Resource
+    GptService gptService;
+
+    /**
+     * 查询某个用户下的所有历史记录
+     * @param userID 用户 ID
+     * @return
+     */
+    @GetMapping("/selectAllByID/{userID}")
+    @ResponseBody
+    public CommonResp selectAll(@PathVariable Long userID) {
+        CommonResp<List<ChatHistory>> resp = new CommonResp<>();
+
+        resp.setContent(gptService.selectAllByID(userID));
+
+        return resp;
+    }
+
+    @GetMapping("/selectContentByID/{historyId}")
+    @ResponseBody
+    public CommonResp<String> selectContentByID(@PathVariable Long historyId) {
+        CommonResp<String> resp = new CommonResp<>();
+        resp.setContent(gptService.selectContentByID(historyId));
+        return resp;
+    }
+}
---
+++ src/main/java/com/jxm/yiti/service/GptService.java
@@ -0,0 +1,85 @@
+package com.jxm.yiti.service;
+
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONArray;
+import com.alibaba.fastjson2.JSONObject;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.jxm.yiti.domain.ChatHistory;
+import com.jxm.yiti.domain.ChatHistoryContent;
+import com.jxm.yiti.domain.ChatHistoryExample;
+import com.jxm.yiti.domain.DownloadList;
+import com.jxm.yiti.mapper.ChatHistoryContentMapper;
+import com.jxm.yiti.mapper.ChatHistoryMapper;
+import com.jxm.yiti.req.ChatCplQueryReq;
+import com.jxm.yiti.resp.ChatCplQueryResp;
+import com.jxm.yiti.utils.SnowFlakeIdWorker;
+import com.theokanning.openai.completion.CompletionRequest;
+import com.theokanning.openai.completion.chat.ChatCompletionRequest;
+import com.theokanning.openai.completion.chat.ChatCompletionResult;
+import com.theokanning.openai.completion.chat.ChatMessage;
+import com.theokanning.openai.completion.chat.ChatMessageRole;
+import com.theokanning.openai.service.OpenAiService;
+import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+@Service
+public class GptService {
+
+    @Resource
+    private ChatHistoryMapper chatHistoryMapper;
+
+    @Resource
+    private ChatHistoryContentMapper chatHistoryContentMapper;
+
+    private static final Logger LOG = LoggerFactory.getLogger(GptService.class);
+
+    /**
+     * 根据用户 ID 查找所有的历史记录
+     */
+    public List<ChatHistory> selectAllByID(Long userID) {
+        List<ChatHistory> res = null;
+        ChatHistoryExample chatHistoryExample = new ChatHistoryExample();
+        ChatHistoryExample.Criteria criteria = chatHistoryExample.createCriteria();
+        criteria.andUserIdEqualTo(userID);
+
+        try {
+            PageHelper.startPage(1, 20, true);
+            res = chatHistoryMapper.selectByExample(chatHistoryExample);
+            PageInfo<ChatHistory> downloadListPageInfo = new PageInfo<>(res);
+            LOG.info("当前页: " + downloadListPageInfo.getPageNum()
+                    + ", 总页数: " + downloadListPageInfo.getPages()
+                    + " , 总记录数: " + downloadListPageInfo.getTotal());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        return res;
+    }
+
+    /**
+     * 根据历史记录 ID 查找对话内容
+     */
+    public String selectContentByID(Long historyId) {
+        ChatHistory chatHistory = chatHistoryMapper.selectByPrimaryKey(historyId);
+        ChatHistoryContent chatHistoryContent = chatHistoryContentMapper.selectByPrimaryKey(chatHistory.getContentId());
+        return chatHistoryContent.getContent();
+    }
+}
