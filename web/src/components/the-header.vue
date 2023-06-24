62dfde9b5b325c04010f49b9c0e8a405bd84c3fb
jiangyk <jiangyk@nwafu.edu.cn>
2023-06-24 19:50:34 +0800

    增加开发和本地的菜单栏显示

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -3,6 +3,7 @@
     <div class="header-menu">
 
       <a-menu
+          v-if="ifShowMenu === 'false'"
           theme="light"
           mode="horizontal"
           class="a-menu"
@@ -16,30 +17,53 @@
         <a-menu-item key="11">
           <router-link to="/chatgpt/Image">3.5 - 画图</router-link>
         </a-menu-item>
-        <!--        <a-menu-item key="2">-->
-        <!--          <router-link to="/download/Download">下载</router-link>-->
-        <!--        </a-menu-item>-->
-        <!--        <a-menu-item key="3">-->
-        <!--          <router-link to="/course/Course">课程</router-link>-->
-        <!--        </a-menu-item>-->
-        <!--        <a-menu-item key="8">-->
-        <!--          <router-link to="/column/Column">专栏</router-link>-->
-        <!--        </a-menu-item>-->
-        <!--        <a-menu-item key="4">-->
-        <!--          <router-link to="/admin/AdminDownload">下载管理</router-link>-->
-        <!--        </a-menu-item>-->
-        <!--        <a-menu-item key="5">-->
-        <!--          <router-link to="/admin/AdminCourse">课程及视频管理</router-link>-->
-        <!--        </a-menu-item>-->
-        <!--        <a-menu-item key="6">-->
-        <!--          <router-link to="/admin/AdminCategory">分类管理</router-link>-->
-        <!--        </a-menu-item>-->
-        <!--        <a-menu-item key="9">-->
-        <!--          <router-link to="/admin/AdminColumn">专栏管理</router-link>-->
-        <!--        </a-menu-item>-->
-        <!--        <a-menu-item key="7">-->
-        <!--          <router-link to="/About">关于</router-link>-->
-        <!--        </a-menu-item>-->
+      </a-menu>
+
+      <a-menu
+          v-if="ifShowMenu === 'true'"
+          theme="light"
+          mode="horizontal"
+          class="a-menu"
+      >
+        <a-menu-item key="1">
+          <router-link to="/Home">首页</router-link>
+        </a-menu-item>
+        <a-menu-item key="10">
+          <router-link to="/chatgpt/Chatgpt">GPT 3.5</router-link>
+        </a-menu-item>
+        <a-menu-item key="11">
+          <router-link to="/chatgpt/Image">3.5 - 画图</router-link>
+        </a-menu-item>
+        <a-menu-item key="2">
+          <router-link to="/download/Download">下载</router-link>
+        </a-menu-item>
+        <a-menu-item key="3">
+          <router-link to="/course/Course">课程</router-link>
+        </a-menu-item>
+        <a-menu-item key="8">
+          <router-link to="/column/Column">专栏</router-link>
+        </a-menu-item>
+        <a-menu-item key="13">
+          <router-link to="/online-problems/Problems">刷题</router-link>
+        </a-menu-item>
+        <a-menu-item key="4">
+          <router-link to="/admin/AdminDownload">下载管理</router-link>
+        </a-menu-item>
+        <a-menu-item key="5">
+          <router-link to="/admin/AdminCourse">课程及视频管理</router-link>
+        </a-menu-item>
+        <a-menu-item key="6">
+          <router-link to="/admin/AdminCategory">分类管理</router-link>
+        </a-menu-item>
+        <a-menu-item key="9">
+          <router-link to="/admin/AdminColumn">专栏管理</router-link>
+        </a-menu-item>
+        <a-menu-item key="12">
+          <router-link to="/admin/AdminQuestion">刷题管理</router-link>
+        </a-menu-item>
+        <a-menu-item key="7">
+          <router-link to="/About">关于</router-link>
+        </a-menu-item>
       </a-menu>
 
     </div>
@@ -227,6 +251,8 @@ export default defineComponent({
     BellTwoTone,
   },
   setup: function () {
+
+    const ifShowMenu = process.env.VUE_APP_SHOW_MENU;
 
     //-------------登录表单--------------
     const loginModalVisible = ref(false);
@@ -579,6 +605,7 @@ export default defineComponent({
       verifyBtnDisable,
       userInfoRefresh,
       descriptionColumn,
+      ifShowMenu,
 
       loginByGitHub,
       loginModalOk,
