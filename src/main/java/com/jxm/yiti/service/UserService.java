df31049e01efe259ad3728892cd3570cb251ff96
jiangyk <jiangyk@nwafu.edu.cn>
2023-09-03 15:04:36 +0800

    修复 "获取每日提问信息" 失败bug: 对于 null String 未处理

--- src/main/java/com/jxm/yiti/service/UserService.java
+++ src/main/java/com/jxm/yiti/service/UserService.java
@@ -700,19 +700,20 @@ public class UserService {
         String nowTime = sdf.format(new Date());
 
         try (Jedis jedis = jedisPool.getResource()) {
-            String ntimes = jedis.get("yt:gpt:ntimes:" + nowTime);
-            String ntokens = jedis.get("yt:gpt:ntokens:" + nowTime);
-            String vtimes = jedis.get("yt:gpt:vtimes:" + nowTime);
-            String vtokens = jedis.get("yt:gpt:vtokens:" + nowTime);
-            String iVtimes = JedisUtil.getIfNotToZero(jedis, "yt:gpti:vtimes:" + nowTime);
+            String ntimes = JedisUtil.getOrZero(jedis, "yt:gpt:ntimes:" + nowTime);
+            String ntokens = JedisUtil.getOrZero(jedis, "yt:gpt:ntokens:" + nowTime);
+            String vtimes = JedisUtil.getOrZero(jedis, "yt:gpt:vtimes:" + nowTime);
+            String vtokens = JedisUtil.getOrZero(jedis, "yt:gpt:vtokens:" + nowTime);
+
+            String iVtimes = JedisUtil.getOrZero(jedis, "yt:gpti:vtimes:" + nowTime);
             res += "当日普通用户: 提问总数: " + ntimes + ", 消耗的总token: " + ntokens + "\n"
                     + "当日会员用户: 提问总数: " + vtimes + ", 消耗的总token: " + vtokens + "\n"
                     + "当日画图使用次数: " + iVtimes + "\n"
-                    + "截止到目前普通用户: 总共消耗: 提问次数: " + (Long.parseLong(jedis.get("yt:gpt:ntimes:ntotal")) + Long.parseLong(ntimes))
-                    + ", 消耗的总token: " + (Long.parseLong(jedis.get("yt:gpt:ntokens:ntotal")) + Long.parseLong(ntokens)) + "\n"
-                    + "截止到目前会员用户: 总共消耗: 提问次数: " + (Long.parseLong(jedis.get("yt:gpt:vtimes:vtotal")) + Long.parseLong(vtimes))
-                    + ", 消耗的总token: " + (Long.parseLong(jedis.get("yt:gpt:vtokens:vtotal")) + Long.parseLong(vtokens)) + "\n"
-                    + "截止到目前画图使用次数: " + (Long.parseLong(jedis.get("yt:gpti:vtimes:vtotal")) + Long.parseLong(iVtimes));
+                    + "截止到目前普通用户: 总共消耗: 提问次数: " + (Long.parseLong(JedisUtil.getOrZero(jedis, "yt:gpt:ntimes:ntotal")) + Long.parseLong(ntimes))
+                    + ", 消耗的总token: " + (Long.parseLong(JedisUtil.getOrZero(jedis, "yt:gpt:ntokens:ntotal")) + Long.parseLong(ntokens)) + "\n"
+                    + "截止到目前会员用户: 总共消耗: 提问次数: " + (Long.parseLong(JedisUtil.getOrZero(jedis, "yt:gpt:vtimes:vtotal")) + Long.parseLong(vtimes))
+                    + ", 消耗的总token: " + (Long.parseLong(JedisUtil.getOrZero(jedis, "yt:gpt:vtokens:vtotal")) + Long.parseLong(vtokens)) + "\n"
+                    + "截止到目前画图使用次数: " + (Long.parseLong(JedisUtil.getOrZero(jedis, "yt:gpti:vtimes:vtotal")) + Long.parseLong(iVtimes));
 
         } catch (Exception e) {
             LOG.error("获取当日的 [提问总数] 与 [消耗的总 token] 失败", e);
--- src/main/java/com/jxm/yiti/utils/JedisUtil.java
+++ src/main/java/com/jxm/yiti/utils/JedisUtil.java
@@ -3,7 +3,9 @@ package com.jxm.yiti.utils;
 import redis.clients.jedis.Jedis;
 
 public class JedisUtil {
-    public static String getIfNotToZero(Jedis jedis, String command) {
+
+    // get 若不存在返回 "0"
+    public static String getOrZero(Jedis jedis, String command) {
         String res = jedis.get(command);
         if (res == null) {
             return "0";
