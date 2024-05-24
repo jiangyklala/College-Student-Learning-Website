c78a1d69cc08d406a3c3640f24a3ffc9ca4a0fdf
jiangyk <jiangyk@nwafu.edu.cn>
2024-05-24 22:33:58 +0800

    fix: cache

--- src/main/java/com/jxm/yiti/controller/WxQuestionController.java
+++ src/main/java/com/jxm/yiti/controller/WxQuestionController.java
@@ -1,5 +1,6 @@
 package com.jxm.yiti.controller;
 
+import com.alibaba.fastjson2.JSON;
 import com.jxm.yiti.annotation.AccessLimit;
 import com.jxm.yiti.enums.WxUserConst;
 import com.jxm.yiti.req.WxQuestionDelReq;
@@ -44,8 +45,9 @@ public class WxQuestionController {
         PageResp<WxQuestionQueryResp> list = wxQuestionService.selectAll(req, false);
 
         resp.setContent(list);
-        log.info("WxQuestionController list, 没有获取到缓存, 更新缓存, key: {}",
-                "wxQuestion_selectAll_" + req.getCategoryId() + '_' + req.getCategoryIdList());
+        log.info("WxQuestionController list, 没有获取到缓存, 更新缓存, key: {} value: {}",
+                "wxQuestion_selectAll_" + req.getCategoryId() + '_' + req.getCategoryIdList(),
+                JSON.toJSONString(list));
         return resp;
     }
 
@@ -79,8 +81,8 @@ public class WxQuestionController {
         PageResp<WxQuestionQueryResp> list = wxQuestionService.selectByCategoryId(req);
 
         resp.setContent(list);
-        log.info("WxQuestionController selectByCategoryId, 没有获取到缓存, 更新缓存, key: {}",
-                "wxQuestion_selectByCategoryId_" + req.getCategoryId());
+        log.info("WxQuestionController selectByCategoryId, 没有获取到缓存, 更新缓存, key: {} value:{}",
+                "wxQuestion_selectByCategoryId_" + req.getCategoryId(), JSON.toJSONString(list));
         return resp;
     }
 
@@ -121,8 +123,8 @@ public class WxQuestionController {
         }
 
         resp.setContent(answer);
-        log.info("WxQuestionController selectAnswer, 没有获取到缓存, 更新缓存, key: {}",
-                "wxQuestion_selectAnswer_" + answerId);
+        log.info("WxQuestionController selectAnswer, 没有获取到缓存, 更新缓存, key: {}, value: {}",
+                "wxQuestion_selectAnswer_" + answerId, JSON.toJSONString(resp));
         return resp;
     }
 
